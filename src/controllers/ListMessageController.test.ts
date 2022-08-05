30c0f447d7f5457cb88d20b6f6b17007f7303d6c
will763 <killzasper@gmail.com>
2022-08-04 23:46:13 -0300

    add test ListMessageController

---
+++ src/controllers/ListMessageController.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest'
+import { createConnection, getConnection } from 'typeorm';
+import { MessagesRepository } from '../repository/MessagesRepository';
+import { app } from '../server';
+import { CreateMessageService } from '../services/CreateMessageService';
+
+describe('CreateMessageController', () => {
+
+    beforeAll(async () => {
+        const connection = await createConnection();
+        connection.runMigrations();
+        const createMessageService = new CreateMessageService(MessagesRepository);
+        await createMessageService.execute({ email: 'test@email.com', message: 'testing' });
+    })
+
+    afterAll(async () => {
+        const connection = getConnection();
+        await connection.query("DELETE FROM messages");
+        await connection.close();
+    })
+
+    test('should able to create a message', () => {
+
+        return request(app).get('/message').then(response => {
+            expect(response.statusCode).toBe(200);
+            expect(response.body[0].email).toEqual('test@email.com');
+            expect(response.body[0].message).toEqual('testing');
+        })
+    })
+
+})
